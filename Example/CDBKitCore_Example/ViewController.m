611da53ea44de65061fc77c9bd3548c1f793078b
truebucha <truebucha@gmail.com>
2017-11-07 11:23:32 +0300

    correct schemes

--- Example/CDBKitCore_Example/ViewController.m
+++ Example/CDBKitCore_Example/ViewController.m
@@ -11,20 +11,42 @@
 
 @interface ViewController ()
 
+@property (copy, nonatomic) CDBArrayErrorCompletion completion;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    weakCDB(weakSelf);
+    self.completion = ^(NSArray * _Nullable array, NSError * _Nullable error) {
+        
+        UILabel * label = [UILabel new];
+        label.frame = weakSelf.view.frame;
+        label.text = array.lastObject;
+        label.font = [UIFont systemFontOfSize: 50.0];
+//        label.textColor = colorWithHex(@"11bb11");
+        [weakSelf.view addSubview:label];
+        
+        strongObjCDB(strongSelf, weakSelf);
+        strongSelf.view.backgroundColor = [UIColor redColor];
+    };
+    
+    [self makeArrayWithCompletion: self.completion];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)makeArrayWithCompletion:(CDBArrayErrorCompletion)completion {
+    if (completion != nil) {
+        completion(@[@"string1", @"222"], nil);
+    }
+}
 
 @end
